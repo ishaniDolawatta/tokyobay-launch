cce1d937d355cd041c5f165ed78401708693dd97
ishaniDolawatta <ishanihans@gmail.com>
2021-05-31 23:49:01 +0530

    initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import companyLogo from './bg.png';
+import button from './button.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+        <img src={companyLogo}  />
+        <a href="http://www.tokyobay.lk/demo/home"><img src={button}  className="button" /></a>
     </div>
   );
 }
